75c46e2056004730e0ce4ef5b526e4c8353dc642
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-12-28 20:22:57 +0000

    chore: other fixes to sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -6,6 +6,8 @@ export default async function sitemap() {
     const routes = ['', '/blog'].map((route) => ({
       url: `${baseUrl}${route}`,
       lastModified: new Date().toISOString().split('T')[0],
+      changeFrequency: 'weekly',
+      priority: 1.0,
     }));
 
     const posts = await getBlogPosts();
@@ -14,12 +16,17 @@ export default async function sitemap() {
       return [...routes];
     }
 
-    const blogs = posts.map((post) => ({
-      url: `${baseUrl}/blog/${post.slug}`,
-      lastModified: new Date(post.metadata.updated || post.metadata.date)
-        .toISOString()
-        .split('T')[0],
-    }));
+    const blogs = posts.map((post) => {
+      const date = post.metadata.updated || post.metadata.date;
+      const lastModified = date
+        ? new Date(date).toISOString().split('T')[0]
+        : new Date().toISOString().split('T')[0];
+      return {
+        url: `${baseUrl}/blog/${post.slug}`,
+        lastModified,
+        priority: 0.5,
+      };
+    });
 
     return [...routes, ...blogs];
 }
