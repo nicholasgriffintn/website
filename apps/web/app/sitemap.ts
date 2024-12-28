a6c3f7dd5f431f4f0adc4970eab4407dc38b87eb
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-12-28 20:17:13 +0000

    chore: adding last modified to sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -3,21 +3,23 @@ import { getBlogPosts } from "@/lib/blog";
 export const baseUrl = "https://nicholasgriffin.dev";
 
 export default async function sitemap() {
-	const routes = ["", "/blog"].map((route) => ({
-		url: `${baseUrl}${route}`,
-		lastModified: new Date().toISOString().split("T")[0],
-	}));
+    const routes = ['', '/blog'].map((route) => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    }));
 
-	const posts = await getBlogPosts();
+    const posts = await getBlogPosts();
 
-	if (!posts) {
-		return [...routes];
-	}
+    if (!posts) {
+      return [...routes];
+    }
 
-	const blogs = posts.map((post) => ({
-		url: `${baseUrl}/blog/${post.slug}`,
-		lastModified: post.metadata.updated || post.metadata.date,
-	}));
+    const blogs = posts.map((post) => ({
+      url: `${baseUrl}/blog/${post.slug}`,
+      lastModified: new Date(post.metadata.updated || post.metadata.date)
+        .toISOString()
+        .split('T')[0],
+    }));
 
-	return [...routes, ...blogs];
+    return [...routes, ...blogs];
 }
