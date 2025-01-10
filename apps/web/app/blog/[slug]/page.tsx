c868ca0b06b9a029a3f24344da90430270af269a
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-01-10 20:12:54 +0000

    fix: stop 00 rendering

--- apps/web/app/blog/[slug]/page.tsx
+++ apps/web/app/blog/[slug]/page.tsx
@@ -104,20 +104,20 @@ export default async function Home({ params }) {
 				<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:gap-6">
 					<div className="col-span-1 order-2 md:order-1 md:col-span-2 lg:col-span-3">
 						<article className="prose dark:prose-invert pt-2 w-full max-w-none">
-							{post.draft && (
+							{post.draft ? (
 								<AlertMessage
 									variant="warning"
 									title="This post is a draft!"
 									description="This post is a draft and may not be finished."
 								/>
-							)}
-							{post.archived && (
+							) : null}
+							{post.archived ? (
 								<AlertMessage
 									variant="warning"
 									title="This post has been archived!"
 									description="This post has been archived due to it being from a previous version of my site, or a bit too old. Some things might be broken and it may not be up to date."
 								/>
-							)}
+							) : null}
 							<div>{parseMarkdown(post.description || "")}</div>
 							<CustomMDX source={post.content} />
 							{post.metadata.link && (
